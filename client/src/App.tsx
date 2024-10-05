fd1f01046c8d2573dc1fb3001a9ddbba15c2380e
Owen Wang <owenwang0099@gmail.com>
2024-10-04 22:20:27 -0400

    init

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+    return (
+        <section className="h-screen">
+            <div className="h-full flex justify-center items-center">
+                <h1 className="text-4xl font-bold">Hows My Form?</h1>
+            </div>
+        </section>
+    );
+}
