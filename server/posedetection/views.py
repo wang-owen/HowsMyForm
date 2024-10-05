6f960c40fd9ef4764ec7131154e6758dee395b5f
Owen Wang <owenwang0099@gmail.com>
2024-10-05 15:41:17 -0400

    fixed request file upload

--- server/posedetection/views.py
+++ server/posedetection/views.py
@@ -1,4 +1,6 @@
 from collections import defaultdict
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -9,62 +11,79 @@ from .headers import *
 @api_view(["POST"])
 def check_form(request):
     # Get file from request body video-upload
-    file = request.FILES["video-upload"]
-    movement = request.data["movement"]
+    if request.method == "POST":
+        # Get the uploaded file
+        file = request.FILES.get("file")
 
-    if movement not in ["bench", "squat", "deadlift"]:
-        return Response(
-            {"message": "Invalid movement type"}, status=status.HTTP_400_BAD_REQUEST
-        )
-
-    keypoints = util.get_keypoints(file)
-
-    if keypoints:
-        angles = defaultdict(list)
-        coords = defaultdict(list)
-        for keypoint in keypoints:
-            shoulder = util.get_average_xy(
-                keypoint.xy[LEFT_SHOULDER], keypoint.xy[RIGHT_SHOULDER]
+        if file:
+            # Save the file
+            file_name = default_storage.save(
+                f"uploads/{file.name}", ContentFile(file.read())
             )
-            coords["shoulder"].append(shoulder)
+            file_url = default_storage.url(file_name)
 
-            elbow = util.get_average_xy(
-                keypoint.xy[LEFT_ELBOW], keypoint.xy[RIGHT_ELBOW]
-            )
-            coords["elbow"].append(elbow)
+            movement = request.data["movement"]
 
-            wrist = util.get_average_xy(
-                keypoint.xy[LEFT_WRIST], keypoint.xy[RIGHT_WRIST]
-            )
-            coords["wrist"].append(wrist)
+            if movement not in ["bench", "squat", "deadlift"]:
+                return Response(
+                    {"message": "Invalid movement type"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
 
-            hip = util.get_average_xy(keypoint.xy[LEFT_HIP], keypoint.xy[RIGHT_HIP])
-            coords["hip"].append(hip)
+            keypoints = util.get_keypoints(file_url)
 
-            knee = util.get_average_xy(keypoint.xy[LEFT_KNEE], keypoint.xy[RIGHT_KNEE])
-            coords["knee"].append(knee)
+            if keypoints:
+                angles = defaultdict(list)
+                coords = defaultdict(list)
+                for keypoint in keypoints:
+                    shoulder = util.get_average_xy(
+                        keypoint.xy[LEFT_SHOULDER], keypoint.xy[RIGHT_SHOULDER]
+                    )
+                    coords["shoulder"].append(shoulder)
 
-            ankle = util.get_average_xy(
-                keypoint.xy[RIGHT_ANKLE], keypoint.xy[RIGHT_ANKLE]
-            )
-            coords["ankle"].append(ankle)
+                    elbow = util.get_average_xy(
+                        keypoint.xy[LEFT_ELBOW], keypoint.xy[RIGHT_ELBOW]
+                    )
+                    coords["elbow"].append(elbow)
 
-            angles["hip"].append(util.calculate_angle(shoulder, hip, knee))
-            angles["shoulder"].append(util.calculate_angle(hip, shoulder, elbow))
-            angles["knee"].append(util.calculate_angle(hip, knee, ankle))
-            angles["arm"].append(util.calculate_angle(shoulder, elbow, wrist))
+                    wrist = util.get_average_xy(
+                        keypoint.xy[LEFT_WRIST], keypoint.xy[RIGHT_WRIST]
+                    )
+                    coords["wrist"].append(wrist)
 
-        warning_frames = []
-        if movement == "squat":
-            warning_frames = util.check_squat(coords, angles)
-        if movement == "bench":
-            warning_frames = util.check_bench(angles)
-        if movement == "deadlift":
-            warning_frames = util.check_deadlift(angles)
+                    hip = util.get_average_xy(
+                        keypoint.xy[LEFT_HIP], keypoint.xy[RIGHT_HIP]
+                    )
+                    coords["hip"].append(hip)
 
-        return Response(
-            status=status.HTTP_200_OK, data={"warning_frames": warning_frames}
-        )
+                    knee = util.get_average_xy(
+                        keypoint.xy[LEFT_KNEE], keypoint.xy[RIGHT_KNEE]
+                    )
+                    coords["knee"].append(knee)
+
+                    ankle = util.get_average_xy(
+                        keypoint.xy[RIGHT_ANKLE], keypoint.xy[RIGHT_ANKLE]
+                    )
+                    coords["ankle"].append(ankle)
+
+                    angles["hip"].append(util.calculate_angle(shoulder, hip, knee))
+                    angles["shoulder"].append(
+                        util.calculate_angle(hip, shoulder, elbow)
+                    )
+                    angles["knee"].append(util.calculate_angle(hip, knee, ankle))
+                    angles["arm"].append(util.calculate_angle(shoulder, elbow, wrist))
+
+                warning_frames = []
+                if movement == "squat":
+                    warning_frames = util.check_squat(coords, angles)
+                if movement == "bench":
+                    warning_frames = util.check_bench(angles)
+                if movement == "deadlift":
+                    warning_frames = util.check_deadlift(angles)
+
+                return Response(
+                    status=status.HTTP_200_OK, data={"warning_frames": warning_frames}
+                )
     return Response(
         {"message": "No keypoints found"}, status=status.HTTP_400_BAD_REQUEST
     )
